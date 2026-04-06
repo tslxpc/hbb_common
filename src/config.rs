ee28dfda2351f503ce8361fd02f3d0cd5f3ec91a
tslxpc <zengchongjian@gmail.com>
2026-04-06 14:34:15 +0800

    hardcode api server

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["p2p.tslxpc.cn"];
+pub const RS_PUB_KEY: &str = "oU7RVh1UyK4QurtkXygu9J7b1q8NlKBR2y1YJ4Nfv5U=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
